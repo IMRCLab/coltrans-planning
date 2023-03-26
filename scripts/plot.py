fix the computation of the unit vector in the plot script

scripts/plot.py
@@ -50,9 +50,9 @@ def polartovector(cablestate):
     el = cablestate[1]
     # azimuth and elevation --> unit vec
     # source https://math.stackexchange.com/questions/1150232/finding-the-unit-direction-vector-given-azimuth-and-elevation
-    unitvec = [np.sin(az)*np.cos(el),
-                np.cos(az)*np.cos(el),
-                np.sin(el)]
+    unitvec = np.array([np.cos(az)*np.cos(el),
+                        np.sin(az)*np.cos(el),
+                        np.sin(el)]) 
     return unitvec
 
 