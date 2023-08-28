baadc0aadd9f3603e10a82410d95154923bf6128
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2023-08-28 12:25:21 +0200

    initguess for dynoplan optimizer script (point mass case only)

--- scripts/init_guess.py
+++ scripts/init_guess.py
@@ -93,38 +93,36 @@ def cableOmega(cables, dt):
 
 
 def unitvecs(cables): 
-    unitvec = []
+    qcs = []
     num_of_cables = int(len(cables[0])/2)
-    for cableNum in range(num_of_cables): 
-        for cab in cables:
-            unitvec.append(polartovector(cab[2*cableNum : 2*cableNum+2])) 
-    return unitvec
+    for cable in cables:
+        tmp = []
+        for cableNum in range(num_of_cables):
+            tmp.append(polartovector(cable[2*cableNum: 2*cableNum+2]))
+        qcs.append(tmp) 
+    return qcs
 
-def qdots(unitvectors, dt, num_of_cables):
-    qds = [[0,0,0]]
-
-    for cableNum in range(num_of_cables):
-        for i in range(len(unitvectors)-1):
-            qi0 = np.asarray(unitvectors[i])
-            qi1 = np.asarray(unitvectors[i+num_of_cables])
-            qds.append(((qi1 - qi0)/dt).tolist())
-    return qds
-            
-def qddots(qdots, dt, num_of_cables):
-    qdds = []
-    for cableNum in range(num_of_cables):
-        for i in range(len(qdots)-1):
-            qdi0 = np.asarray(qdots[i])
-            qdi1 = np.asarray(qdots[i+num_of_cables])
-            qdds.append(((qdi1 - qdi0)/dt).tolist())
-    return qdds
+def qdots(qcs, dt, num_of_cables):
+    qcsd = [[np.zeros(3,).tolist() for cableNum in range(num_of_cables)]]
+    for idx, qc in enumerate(qcs[0:-1]): 
+        tmp = []
+        for cableNum in range(num_of_cables):
+            qc0 = np.array(qc[cableNum])
+            qc1 = np.array(qcs[idx+1][cableNum])
+            tmp.append(((qc1 - qc0)/dt).tolist())
+        qcsd.append(tmp)
+    return qcsd
             
 
-def omegaCable(qdts, unitvectors):
-    omega = []
-
-    for qd, qi in zip(qdts, unitvectors):
-        omega.append(np.cross(qi,qd).tolist())
+def omegaCable(qcs, qcsd, num_of_cables):
+    omega = [[np.zeros(3,).tolist() for cabeNum in range(num_of_cables)]] # initial state
+    for qc, qcd in zip(qcs[0:-1], qcsd[0:-1]):
+        tmp = []
+        for cableNum in range(num_of_cables):
+            qci = np.array(qc[cableNum])
+            qcdi = np.array(qcd[cableNum])
+            tmp.append(np.cross(qci, qcdi).tolist())
+        omega.append(tmp)
     return omega
 
 
@@ -134,21 +132,22 @@ def polartovector(cablestate):
     el = cablestate[1]
     # azimuth and elevation --> unit vec
     # source https://math.stackexchange.com/questions/1150232/finding-the-unit-direction-vector-given-azimuth-and-elevation
-    unitvec = [mt.cos(az)*mt.sin(el),
+    unitvec = [mt.cos(az)*mt.cos(el),
                 mt.sin(az)*mt.cos(el),
-                mt.cos(el)]
+                -mt.sin(el)]
     return unitvec
-
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--inp", type=str, help="yaml file for the table")
     parser.add_argument("--out", default=None,  type=str, help="yaml file for the table")
+    parser.add_argument("-w", "--write", action="store_true")  # on/off flag: write yaml file
+
     args = parser.parse_args()
+    write = args.write
 
     dt = 1/FREQUENCY    
-    l  = 0.25
     inp = args.inp
     out = args.out
 
@@ -156,49 +155,53 @@ def main():
         plan = yaml.load(f, Loader=yaml.FullLoader) 
 
     states = plan["result"][0]["states"]
-    
+    l = plan["result"][1]["cablelengths"]
+    attPts = plan["result"][2]["cablepoints"]
+    payloadType = plan["result"][4]["payload"][0]["type"]
+
     # payload position
-    position = np.asarray([state[0:3] for state in states])
-   
+    p_load = np.asarray([state[0:3] for state in states])   
     # payload velocity 
-    velocity = derivative(position, dt)
-    
-    acceleration = derivative(position, dt)
+    v_load = derivative(p_load, dt)
+
     # cables states
-    cables  = np.asarray([state[7::] for state in states])
-    num_of_cables = int(len(cables[0])/2)
+    cableSt = np.empty(3,)
+    if payloadType == "sphere":
+        cableSt  = np.asarray([state[7::] for state in states])
+    elif payloadType == "rigid":
+        cableSt  = np.asarray([state[13::] for state in states])
+    else:
+        print("payload type is wrong please check it: sphere or rigid")
+        exit()
+    
+    num_of_cables = int(len(cableSt[0])/2)
 
     # cable directional vectors
-    unitvectors = unitvecs(cables)
-    
+    qcs = unitvecs(cableSt)
     # cable directional derivative
-    qdts = qdots(unitvectors, dt, num_of_cables)
-    qddts = qddots(qdts, dt, num_of_cables)
-    cable_omegas = omegaCable(qdts, unitvectors)
-    # uav position  
-    uavPos = cable2uavPos(position, unitvectors, l, num_of_cables)
-    uavVel = cable2uavVel(velocity, qdts, l, num_of_cables)
-    uavAcc = cable2uavAcc(acceleration, qddts, l, num_of_cables)
-
+    qcsd = qdots(qcs, dt, num_of_cables)
+    cable_omegas = omegaCable(qcs, qcsd, num_of_cables)
+   
     finalStates = []
     actions = []
-    count = 0
-    for pos, vel, acc, uvec, om in zip(uavPos, uavVel, uavAcc, unitvectors, cable_omegas):
-        th = 0
-        yaw = mt.atan2(uvec[0], uvec[2])
-        d = 1
-        M = np.array([[1, 1], [d, -d]])
-        if count != 0:
-            # m = 2+0.25
-            # act_vec = np.asarray([acc[0],vel[2]+ m*9.81])
-            # act = act_vec/np.linalg.norm(act_vec)
-            # th = mt.atan2(act_vec[0],act_vec[1])
-            # f = np.linalg.norm(act)
-            # f1f2 = np.linalg.inv(M)@np.array([f, th])
-            # actions.append([float(f1f2[0]),float(f1f2[1])])
-            actions.append([1,1])
-        finalStates.append([pos[0],pos[2], 0, 0, 0, 0, 0, 0])
-        count+=1
+    oldStateRep = False
+    for i, (pos, vel, qc, wc) in enumerate(zip(p_load, v_load, qcs, cable_omegas)):
+        if i < len(v_load)-1:
+            actions.append(np.ones(4*num_of_cables,).tolist())
+        quat = [0,0,0,1]
+        w = [0,0,0]
+        if oldStateRep: 
+            finalState = [*pos.tolist(), *qc[0], *vel.tolist(), *wc[0], *quat, 0,0,0]
+            finalStates.append(finalState)
+        else:
+            tmp = []
+            for qc_, wc_ in zip(qc, wc): 
+                tmp.extend(qc_)
+                tmp.extend(wc_)
+                tmp.extend(quat)
+                tmp.extend(w)
+            finalState = [*pos.tolist(), *vel.tolist(), *tmp]
+            finalStates.append(finalState)
     output = {}
     output["feasible"] = 0
     output["cost"] = 10
@@ -206,7 +209,9 @@ def main():
     output["result"]["states"] = finalStates
     output["result"]["actions"] = actions 
 
-    with open(args.out, 'w') as file:
-        yaml.safe_dump(output, file, default_flow_style=None)
+    if write:
+        with open(args.out, 'w') as file:
+            yaml.safe_dump(output, file, default_flow_style=None)
+
 if __name__ == "__main__":
     main()
