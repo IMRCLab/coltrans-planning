f14a4a96f47a7a4e44b49baf53d000e1c37717c4
Wolfgang Hoenig <hoenig@tu-berlin.de>
2023-09-12 15:00:26 +0200

    checker: guard against failed runs

--- scripts/report_info.py
+++ scripts/report_info.py
@@ -30,8 +30,10 @@ class Report:
                 act_traj_path = result_folder/ "trajectory_opt.yaml"
                 check_traj_path = result_folder/ "trajectory_opt.check.txt"
 
-            with open(check_traj_path, "r") as f:
-                solution_valid = (f.readlines()[-1][0:2] == "OK")
+            solution_valid = False
+            if check_traj_path.exists():
+                with open(check_traj_path, "r") as f:
+                    solution_valid = (f.readlines()[-1][0:2] == "OK")
             
             if ref_traj_path.exists() and act_traj_path.exists() and solution_valid:
                 with open(ref_traj_path, "r") as ref_file:
