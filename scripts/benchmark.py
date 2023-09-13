66d152fa81e6203d5e91f9613c5b8007f5ad393a
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2023-09-13 17:25:45 +0200

    remove comments

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -251,23 +251,23 @@ def execute_task(task: ExecutionTask):
 def main():
 	parallel = True
 	instances = [
-		# { "name": "empty_2robots", "models_path": "point_2.yaml"},
-		# { "name": "empty_3robots", "models_path": "point_3.yaml"},
-		# { "name": "empty_4robots", "models_path": "point_4.yaml"},
-		# { "name": "empty_5robots", "models_path": "point_5.yaml"},
-		# { "name": "empty_6robots", "models_path": "point_6.yaml"},
+		{ "name": "empty_2robots", "models_path": "point_2.yaml"},
+		{ "name": "empty_3robots", "models_path": "point_3.yaml"},
+		{ "name": "empty_4robots", "models_path": "point_4.yaml"},
+		{ "name": "empty_5robots", "models_path": "point_5.yaml"},
+		{ "name": "empty_6robots", "models_path": "point_6.yaml"},
 
 		{ "name": "forest_2robots", "models_path": "point_2.yaml"},
 		{ "name": "forest_3robots", "models_path": "point_3.yaml"},
-		# { "name": "forest_4robots", "models_path": "point_4.yaml"},
-		# { "name": "forest_5robots", "models_path": "point_5.yaml"},
-		# { "name": "forest_6robots", "models_path": "point_6.yaml"},
+		{ "name": "forest_4robots", "models_path": "point_4.yaml"},
+		{ "name": "forest_5robots", "models_path": "point_5.yaml"},
+		{ "name": "forest_6robots", "models_path": "point_6.yaml"},
 
-		# { "name": "maze_2robots", "models_path": "point_2.yaml"},
-		# { "name": "maze_3robots", "models_path": "point_3.yaml"},
-		# { "name": "maze_4robots", "models_path": "point_4.yaml"},
-		# { "name": "maze_5robots", "models_path": "point_5.yaml"},
-		# { "name": "maze_6robots", "models_path": "point_6.yaml"},
+		{ "name": "maze_2robots", "models_path": "point_2.yaml"},
+		{ "name": "maze_3robots", "models_path": "point_3.yaml"},
+		{ "name": "maze_4robots", "models_path": "point_4.yaml"},
+		{ "name": "maze_5robots", "models_path": "point_5.yaml"},
+		{ "name": "maze_6robots", "models_path": "point_6.yaml"},
 	]
 	algs = [
 		# "geom",
