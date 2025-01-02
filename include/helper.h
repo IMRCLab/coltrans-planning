a681e9f4bbc301ad29d94ea5cd9919c989a39157
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-01-02 02:12:07 +0200

    add helpers

--- include/helper.h
+++ include/helper.h
@@ -10,3 +10,5 @@ Eigen::VectorXf yamltoEigen(const YAML::Node &yamlvec);
 Eigen::VectorXf stdtoEigen(const std::vector<double>& stdvec);
 
 std::vector<double> eigentoStd(const Eigen::VectorXf &eigenvec);
+
+bool startsWith(const std::string &str, const std::string &prefix);
