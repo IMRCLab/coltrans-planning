0b78108c75f94cafd17c601394413372046b2410
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-01-14 12:11:14 +0100

    add condition to draw ref trajectory if provided

--- scripts/visualize_unicycles.py
+++ scripts/visualize_unicycles.py
@@ -63,26 +63,26 @@ def visualize(env_file, result_file, video_file, reference_traj_file=None):
     states = np.array(states)
     num_robots = data["robots"][0]["quadsNum"]
 
+    if reference_traj_file is not None:
+        # tracing:
+        px_trace, py_trace, alpha1_trace = states[:,0].copy(), states[:,1].copy(), states[:,2].copy()
+        px_traced, py_traced, alpha1_traced = states_d[:,0].copy(), states_d[:,1].copy(), states_d[:,2].copy()
+        l = 0.5
+        # if reference_traj_file is not None: 
+        #     pxd, pyd, alphad = states_d[:,0], states_d[:,1], states_d[:,2]
+        vis[f"trace_0"].set_object(g.Line(g.PointsGeometry( np.array([px_trace,py_trace])), g.LineBasicMaterial(color=0xffffff)))
+        vis[f"trace_d0"].set_object(g.Line(g.PointsGeometry( np.array([px_traced,py_traced])), g.LineBasicMaterial(color=0xff0022)))
 
-    # tracing:
-    px_trace, py_trace, alpha1_trace = states[:,0].copy(), states[:,1].copy(), states[:,2].copy()
-    px_traced, py_traced, alpha1_traced = states_d[:,0].copy(), states_d[:,1].copy(), states_d[:,2].copy()
-    l = 0.5
-    # if reference_traj_file is not None: 
-    #     pxd, pyd, alphad = states_d[:,0], states_d[:,1], states_d[:,2]
-    vis[f"trace_0"].set_object(g.Line(g.PointsGeometry( np.array([px_trace,py_trace])), g.LineBasicMaterial(color=0xffffff)))
-    vis[f"trace_d0"].set_object(g.Line(g.PointsGeometry( np.array([px_traced,py_traced])), g.LineBasicMaterial(color=0xff0022)))
-
-    for i in range(num_robots-1):
-        th_cable_trace = states[:,2 + num_robots + i]
-        px_trace +=  l*np.cos(th_cable_trace)
-        py_trace +=  l*np.sin(th_cable_trace)
-        th_cable_traced = states_d[:,2 + num_robots + i]
-        px_traced +=  l*np.cos(th_cable_traced)
-        py_traced +=  l*np.sin(th_cable_traced)
-        
-        vis[f"trace_{i+1}"].set_object(g.Line(g.PointsGeometry( np.array([px_trace,py_trace])), g.LineBasicMaterial(color=0xffffff)))
-        vis[f"trace_d{i+1}"].set_object(g.Line(g.PointsGeometry( np.array([px_traced,py_traced])), g.LineBasicMaterial(color=0xff0022)))
+        for i in range(num_robots-1):
+            th_cable_trace = states[:,2 + num_robots + i]
+            px_trace +=  l*np.cos(th_cable_trace)
+            py_trace +=  l*np.sin(th_cable_trace)
+            th_cable_traced = states_d[:,2 + num_robots + i]
+            px_traced +=  l*np.cos(th_cable_traced)
+            py_traced +=  l*np.sin(th_cable_traced)
+            
+            vis[f"trace_{i+1}"].set_object(g.Line(g.PointsGeometry( np.array([px_trace,py_trace])), g.LineBasicMaterial(color=0xffffff)))
+            vis[f"trace_d{i+1}"].set_object(g.Line(g.PointsGeometry( np.array([px_traced,py_traced])), g.LineBasicMaterial(color=0xff0022)))
 
     # Visualize robots and rods
     for i in range(num_robots):
@@ -140,7 +140,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--robot', type=str, help="robot model: unicycle")
     parser.add_argument('--env', type=str, help="environment")
-    parser.add_argument('--ref', default=None, type=str, help="reference trajectory")
+    parser.add_argument('--ref',  type=str, help="reference trajectory")
     parser.add_argument('--result', type=str, help="result trajectory")
     parser.add_argument('--output', type=str, help="html animation")
     parser.add_argument("-i", "--interactive", action="store_true")  # on/off flag
